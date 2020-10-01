2666bebffedd3158772037a63e35a688ec31cc28
haleozcan <haleeozcan@github.com>
2020-10-01 17:36:38 +0300

    Header Adding

---
+++ src/Header.js
@@ -0,0 +1,103 @@
+import React from 'react';
+import {makeStyles} from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import Switch from '@material-ui/core/Switch';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormGroup from '@material-ui/core/FormGroup';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+    title: {
+        flexGrow: 1,
+    },
+}));
+
+export default function MenuAppBar() {
+    const [anchor, setAnchor] = React.useState(null);
+
+    const handleOnClick = (event) => {
+        setAnchor(event.currentTarget);
+    };
+
+    const handleOnClose = () => {
+        setAnchor(null);
+    };
+
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const classes = useStyles();
+    const [auth, setAuth] = React.useState(true);
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
+
+    const handleChange = (event) => {
+        setAuth(event.target.checked);
+    };
+
+    const handleMenu = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    return (
+        <div className={classes.root}>
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                        <MenuIcon/>
+                    </IconButton>
+                    <div>
+                        <Menu
+                            id="menu-appbar"
+                            anchorEl={anchorEl}
+                            anchorOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                            }}
+                            open={open}
+                            onClose={handleClose}
+                        >
+                            <MenuItem onClick={handleClose}></MenuItem>
+                        </Menu>
+                    </div>
+                    <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleOnClick}>
+                        Dil
+                    </Button>
+                    <Menu
+                        id="simple-menu"
+                        anchorEl={anchor}
+                        keepMounted
+                        open={Boolean(anchor)}
+                        onClose={handleOnClose}
+                    >
+                        <MenuItem onClick={handleOnClose}>Profile</MenuItem>
+                        <MenuItem onClick={handleOnClose}>My account</MenuItem>
+                        <MenuItem onClick={handleOnClose}>Logout</MenuItem>
+                    </Menu>
+                </Toolbar>
+            </AppBar>
+        </div>
+    );
+}
